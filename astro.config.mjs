c19b7688f1e0be808eac8ec5169697cb1c256f52
Kai Richardson <krichardson43@outlook.com>
2025-10-02 18:12:48 +0100

    done for the night

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   output: 'static',
   site: 'https://kairicho.github.io',
-  base: '/resume/',
   integrations: [tailwind()]
 });
